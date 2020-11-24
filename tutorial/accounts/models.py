7941f6ff23710b5ff48a7d79bb31628ab21c223a
maazil3861 <maazshaikh7020@gmail.com>
2020-11-24 22:44:30 +0530

    user data is save in UserProfile db using User,post_save

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
@@ -10,3 +11,9 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone  = models.IntegerField(default=0)
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        Users_Profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
