3459f53823a5541477b4cc6bf3c2fc518ab82a40
maazil3861 <maazshaikh7020@gmail.com>
2020-11-25 17:23:34 +0530

    if create custom form then UserCreationFrom in comments

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login , logout
+
 
 urlpatterns = [
     url(r'^$', views.homepage),
     url(r'^welcome/', views.welcome),
     url(r'value/', views.passing_value),
     url(r'override/', views.override),
-    url(r'^login/',login, {'template_name': 'accounts/login.html'})
+    url(r'^login/',login, {'template_name': 'accounts/login.html'}),
+    url(r'^logout/',logout, {'template_name': 'accounts/logout.html'}),
+    # url(r'register/', views.register),
+    url(r'^custom_register/', views.custom_register)
 ]
