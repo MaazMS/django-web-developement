270560fe591ec1b01045408d2fbc532964d78393
maazil3861 <maazshaikh7020@gmail.com>
2020-11-26 18:39:02 +0530

    Edit_personal_details for edit personal user information

--- tutorial/accounts/forms.py
+++ tutorial/accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , UserChangeForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -18,10 +18,22 @@ class RegistrationForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
-        user.first_name=self.cleaned_date['first_name']
-        user.last_name = self.cleaned_date['last_name']
-        user.email = self.cleaned_date['email']
+        user.first_name=self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
 
         if commit:
             user.save()
         return user
+
+class Edit_personal_details(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password'
+
+        )
