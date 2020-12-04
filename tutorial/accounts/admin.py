ebcccbb76cf7b5bee70993f86c376179a1d87ab9
maazil3861 <maazshaikh7020@gmail.com>
2020-12-04 18:00:11 +0530

    change value by order and display it

--- tutorial/accounts/admin.py
+++ tutorial/accounts/admin.py
@@ -4,13 +4,16 @@ from  . models import UserProfile
 # Register your models here.
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','user_info', 'city', 'phone', 'website')
-
-    # user_info is the description
+    list_display = ('user', 'user_info', 'city', 'phone', 'website')
 
     def user_info(self, obj):
         return obj.description
+    user_info.short_description =  'info'
 
+    def get_queryset(self, request):
+        queryset = super(UserProfileAdmin, self).get_queryset(request)
+        queryset =queryset.order_by('-phone')
+        return queryset
 
 admin.site.register(UserProfile, UserProfileAdmin)
 # admin.site.site_header = "Administration "
