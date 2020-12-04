4d6b4a7f613b7102052798a79fd2b919c413f749
maazil3861 <maazshaikh7020@gmail.com>
2020-12-04 17:13:11 +0530

    show all column name and also change the column and display content

--- tutorial/accounts/admin.py
+++ tutorial/accounts/admin.py
@@ -3,5 +3,15 @@ from  . models import UserProfile
 
 # Register your models here.
 
-admin.site.register(UserProfile)
-admin.site.site_header = "Administration "
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','user_info', 'city', 'phone', 'website')
+
+    # user_info is the description
+
+    def user_info(self, obj):
+        return obj.description
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+# admin.site.site_header = "Administration "
+
