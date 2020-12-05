d84b73e2b8ba5ad0002270b02cdf06f0949ecea4
maazil3861 <maazshaikh7020@gmail.com>
2020-12-05 07:28:39 +0530

    how directly access column value and then manage manipulate data

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
-# Create your models here.
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager, self).get_queryset().filter(city='pune')
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -11,9 +14,16 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone  = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_images', blank=True)
 
+    def __str__(self):
+        return self.user.username
+
+    pune = UserProfileManager()
 def create_profile(sender, **kwargs):
     if kwargs['created']:
         Users_Profile = UserProfile.objects.create(user=kwargs['instance'])
 
 post_save.connect(create_profile, sender=User)
+
+
