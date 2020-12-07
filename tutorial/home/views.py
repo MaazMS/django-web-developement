043e824429e5b73d6097f5dfd67cfb31cfc6e354
maazil3861 <maazshaikh7020@gmail.com>
2020-12-07 15:44:04 +0530

    get and post method for user defined form

--- tutorial/home/views.py
+++ tutorial/home/views.py
@@ -1,18 +1,23 @@
 from django.views.generic import TemplateView
 from home.forms import HomeForm
 from django.shortcuts import render
+from home.models import Post
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name , {'form': form})
+        posts = Post.objects.all()
+        return render(request, self.template_name , {'form': form, 'posts': posts})
 
     def post(self, request):
         form = HomeForm(request.POST)
         if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
             text = form.cleaned_data['post']
             form = HomeForm()
-        args = {'form': form , 'text' : text}
-        return render(request, self.template_name, args)
+            args = {'form': form, 'text': text}
+            return render(request, self.template_name, args)
