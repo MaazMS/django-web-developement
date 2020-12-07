c037cfea0a7dfdd8635a970a0a48342daf695806
maazil3861 <maazshaikh7020@gmail.com>
2020-12-07 15:41:53 +0530

    create user defined form

--- tutorial/home/forms.py
+++ tutorial/home/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from home.models import Post
 
-class HomeForm(forms.Form):
+class HomeForm(forms.ModelForm):
     post = forms.CharField(required= False)
+
+    class Meta:
+        model = Post
+        fields = ('post',)
